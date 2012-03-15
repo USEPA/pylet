1878a8daa670901c44a5bd083f859f07b28c78fc
Michael Jackson <jackson.michael@epa.gov>
2012-03-14 19:05:31 -0700

    arcgis10 function to get list of all parameters as text

--- pylet/arcgis10/__init__.py
+++ pylet/arcgis10/__init__.py
@@ -1,1 +1,22 @@
 import fields
+
+import arcpy as _arcpy
+
+def getParametersAsText():
+    """ Returns list of all arcpy parameters as text
+    
+    """
+    
+    count = 0
+    textParameters = []
+    
+    while True:
+        try:
+            textParameters.append(_arcpy.GetParameterAsText(count))
+        except: 
+            break
+    
+        
+        count += 1
+    
+    return textParameters   
