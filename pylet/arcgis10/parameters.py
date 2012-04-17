010ac280398a7a9abac0e1251ee9b641030f520b
Michael Jackson <jackson.michael@epa.gov>
2012-04-16 17:36:49 -0700

    arcgis10.parameters module added

---
+++ pylet/arcgis10/parameters.py
@@ -0,0 +1,50 @@
+''' arcpy helper utilities specific to parameters
+
+'''
+
+import arcpy
+
+PARAMETER_ENCLOSURE = "'"
+
+
+
+def getParametersAsText():
+    """ Returns list of all arcpy parameters as text
+    
+    """
+    
+    count = 0
+    textParameters = []
+    
+    while True:
+        try:
+            textParameters.append(arcpy.GetParameterAsText(count))
+        except: 
+            break
+    
+        
+        count += 1
+    
+    return textParameters   
+
+
+def splitItemsAndStripDescriptions(delimitedString, descriptionDelim, parameterDelim=";"):
+    """ Splits string with delimited item+description to list of items.
+    
+        The expected input is a string with nested delimiters, with the following format: 
+        'item<descriptionDelim>description<parameterDelim>item<descriptionDelim>description'
+        e.g., 'for  [pfor] Forest;wetl  [pwetl]  wetland' --> ['for','wetl']
+        
+        delimitedString:  the full delimited string
+        descriptionDelim: The delimeter for item descriptions.  Descriptions are stripped off
+        parmeterDelim: The delimiter for parameters.  The default is a semi-colon.
+        
+    """
+    
+    delimitedString = delimitedString.replace(PARAMETER_ENCLOSURE,"")
+    
+    itemsWithDescription = delimitedString.split(parameterDelim)
+    
+    itemsWithoutDescription = [itemWithDescription.split(descriptionDelim)[0] for itemWithDescription in itemsWithDescription]
+    
+    return itemsWithoutDescription
