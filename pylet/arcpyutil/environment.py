b534d322670800527d63bf084762bd326692b642
hultgren.torrin <hultgren.torrin@epa.gov>
2013-09-12 17:43:41 -0400

    Encountered issue with spaces in temporary workspace path - surprised this is still an issue.  Implemented fix to avoid paths with spaces and alert user if none of the available scratch workspaces are space free.

--- pylet/arcpyutil/environment.py
+++ pylet/arcpyutil/environment.py
@@ -8,7 +8,7 @@
 """
 
 import tempfile
-
+import arcpy
 from arcpy import env
 
 
@@ -25,6 +25,8 @@ def getWorkspaceForIntermediates(fallBackWorkspace=None):
         3. `System Temp`_ - Based on system variables and existing folders
         4. *fallBackWorkspace* - The default is None.
          
+        It seems that some tools still have issues with spaces in the pathname, so that test will be added.
+        
         
     **Arguments:**
         
@@ -39,18 +41,31 @@ def getWorkspaceForIntermediates(fallBackWorkspace=None):
     
     # Scratch workspace from ArcGIS Environments
     scratchWorkspace = env.scratchWorkspace
-    if scratchWorkspace:
+    if spaceCheck(scratchWorkspace):
         return scratchWorkspace
     
     # Current workspace from ArcGIS Environments
     currentWorkspace = env.workspace
-    if currentWorkspace:
+    if spaceCheck(currentWorkspace):
         return currentWorkspace
     
     # System temp directory
     systemTempWorkspace = tempfile.gettempdir()
-    if systemTempWorkspace:
+    if spaceCheck(systemTempWorkspace):
         return systemTempWorkspace
     
     # User supplied directory or default None
-    return fallBackWorkspace
+    if spaceCheck(fallBackWorkspace):
+        return fallBackWorkspace
+    else:
+        msg = """All available temp workspaces are either null or contain spaces, which may cause errors. Please set the 
+        ScratchWorkspace geoprocessing environment setting to a directory or file geodatabase whose full path contains no spaces."""
+        arcpy.AddMessage(msg)
+        
+
+def spaceCheck(path):
+    """Returns true if path is not null and does not contain spaces, otherwise returns false"""
+    if path and not " " in path:
+        return True
+    else: 
+        return False
